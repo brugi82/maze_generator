8f3f8c741514ffc6401f7de899fb38bc079f8f31
Milos Petrovic <milos.brugi@gmail.com>
2019-09-27 11:48:37 -0400

    Moved code for image drawing. Cleanup.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod maze;
 extern crate image;
 
-use maze::maze::{ Maze, BorderType };
+use maze::maze::{ Maze };
 use std::io;
 
 fn main() {
@@ -26,69 +26,7 @@ fn main() {
     let mut maze = Maze::new(width, height);
     maze.generate_maze();
 
-    let imgx = resolution * width;
-    let imgy = resolution * height;
-
-
-    let mut imgbuf = image::ImageBuffer::from_fn(imgx, imgy, |_x, _y| {
-        image::Rgb([248, 248, 248])
-    });
-
-    for row in maze.cells.iter() {
-        for cell in row.iter() {
-            if let BorderType::Wall = cell.up {
-                for pixel_index in 0..resolution {
-                    let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution, pixel_index + resolution * cell.col_index);
-                    let image::Rgb(_data) = *pixel;
-                    *pixel = image::Rgb([128, 128, 128]);
-                }
-            }
-
-            if let BorderType::Wall = cell.down {
-                for pixel_index in 0..resolution {
-                    let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + resolution - 1, pixel_index + resolution * cell.col_index);
-                    let image::Rgb(_data) = *pixel;
-                    *pixel = image::Rgb([128, 128, 128]);
-                }
-            }
-
-            if let BorderType::Wall = cell.left {
-                for pixel_index in 0..resolution {
-                    let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + pixel_index, resolution * cell.col_index);
-                    let image::Rgb(_data) = *pixel;
-                    *pixel = image::Rgb([128, 128, 128]);
-                }
-            }
-
-            if let BorderType::Wall = cell.right {
-                for pixel_index in 0..resolution {
-                    let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + pixel_index, resolution * cell.col_index + resolution - 1);
-                    let image::Rgb(_data) = *pixel;
-                    *pixel = image::Rgb([128, 128, 128]);
-                }
-            }
-        }
-    }
-
-    let start_row = maze.start_position.0;
-    let start_col = maze.start_position.1;
-
-    for pixel_row in 1..(resolution - 1) {
-        for pixel_col in 1..(resolution - 1) {
-            if ((pixel_row + pixel_col) % 4 == 0) || ((pixel_row + pixel_col + 1) % 4 == 0) {
-                update_start_cells(start_row * resolution + pixel_row, start_col * resolution + pixel_col, &mut imgbuf);
-                update_start_cells(pixel_row, pixel_col, &mut imgbuf);
-            }
-        }
-    }
-
-    imgbuf.save("maze.png").unwrap();
-}
-
-fn update_start_cells(pixel_row: u32, pixel_col: u32, imgbuf: &mut image::ImageBuffer<image::Rgb<u8>, Vec<u8>>) {
-    let start_pixel = imgbuf.get_pixel_mut(pixel_row, pixel_col);
-    let image::Rgb(_data) = *start_pixel;
-    *start_pixel = image::Rgb([252, 3, 94]);
+    maze.draw(resolution, "maze.png".parse().unwrap());
 }
 
 
--- src/maze.rs
+++ src/maze.rs
@@ -1,5 +1,6 @@
 pub mod maze {
     use rand::Rng;
+    extern crate image;
 
     pub enum BorderType {
         Passage,
@@ -99,6 +100,66 @@ pub mod maze {
             }
         }
 
+        pub fn draw(&self, resolution: u32, path: String) {
+            let imgx = resolution * self.width;
+                let imgy = resolution * self.height;
+
+
+                let mut imgbuf = image::ImageBuffer::from_fn(imgx, imgy, |_x, _y| {
+                    image::Rgb([248, 248, 248])
+                });
+
+                for row in self.cells.iter() {
+                    for cell in row.iter() {
+                        if let BorderType::Wall = cell.up {
+                            for pixel_index in 0..resolution {
+                                let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution, pixel_index + resolution * cell.col_index);
+                                let image::Rgb(_data) = *pixel;
+                                *pixel = image::Rgb([128, 128, 128]);
+                            }
+                        }
+
+                        if let BorderType::Wall = cell.down {
+                            for pixel_index in 0..resolution {
+                                let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + resolution - 1, pixel_index + resolution * cell.col_index);
+                                let image::Rgb(_data) = *pixel;
+                                *pixel = image::Rgb([128, 128, 128]);
+                            }
+                        }
+
+                        if let BorderType::Wall = cell.left {
+                            for pixel_index in 0..resolution {
+                                let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + pixel_index, resolution * cell.col_index);
+                                let image::Rgb(_data) = *pixel;
+                                *pixel = image::Rgb([128, 128, 128]);
+                            }
+                        }
+
+                        if let BorderType::Wall = cell.right {
+                            for pixel_index in 0..resolution {
+                                let pixel = imgbuf.get_pixel_mut(cell.row_index * resolution + pixel_index, resolution * cell.col_index + resolution - 1);
+                                let image::Rgb(_data) = *pixel;
+                                *pixel = image::Rgb([128, 128, 128]);
+                            }
+                        }
+                    }
+                }
+
+                let start_row = self.start_position.0;
+                let start_col = self.start_position.1;
+
+                for pixel_row in 1..(resolution - 1) {
+                    for pixel_col in 1..(resolution - 1) {
+                        if ((pixel_row + pixel_col) % 4 == 0) || ((pixel_row + pixel_col + 1) % 4 == 0) {
+                            self.update_start_cells(start_row * resolution + pixel_row, start_col * resolution + pixel_col, &mut imgbuf);
+                            self.update_start_cells(pixel_row, pixel_col, &mut imgbuf);
+                        }
+                    }
+                }
+
+                imgbuf.save(path).unwrap();
+        }
+
         fn get_available_moves(&self) -> Vec<MoveDirection> {
             let mut available_moves: Vec<MoveDirection> = vec![];
 
@@ -192,5 +253,13 @@ pub mod maze {
                 self.max_len += 1;
             }
         }
+
+        fn update_start_cells(&self, pixel_row: u32, pixel_col: u32, imgbuf: &mut image::ImageBuffer<image::Rgb<u8>, Vec<u8>>) {
+            let start_pixel = imgbuf.get_pixel_mut(pixel_row, pixel_col);
+            let image::Rgb(_data) = *start_pixel;
+            *start_pixel = image::Rgb([252, 3, 94]);
+        }
     }
+
+
 }
